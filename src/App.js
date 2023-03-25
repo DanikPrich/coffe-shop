b3002ed835e8bf5940c33c2c31ff5cc32772d18a
Daniel <Danil.danek@gmail.com>
2023-03-25 09:48:18 +0100

    goods item created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import HeaderMainSection from './components/sections/header-main-section/header-main-section'
 import AboutMainSection from './components/sections/about-main-section/about-main-section'
+import GoodsItem from './components/UI/goods-item/goods-item'
 
 import './App.scss';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <HeaderMainSection/>
       <AboutMainSection/>
+      <GoodsItem/>
     </div>
   );
 }
