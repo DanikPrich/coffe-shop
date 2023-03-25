e4017e0bd3fe7744eb6893f5b10ccad4728afb08
Daniel <Danil.danek@gmail.com>
2023-03-25 14:16:30 +0100

    goods item page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import MainPage from './pages/main-page';
 import CoffePage from './pages/coffe-page';
 import GoodsPage from './pages/goods-page';
+import GoodsItemPage from './pages/goods-item-page';
 
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <GoodsPage/>
+      <GoodsItemPage/>
     </div>
   );
 }
---
+++ src/pages/goods-item-page.js
@@ -0,0 +1,18 @@
+
+import HeaderSection from "../components/sections/header-section/header-section"
+import FooterSection from "../components/sections/footer-section/footer-section"
+import AboutSection from "../components/sections/about-section/about-section"
+import SearchSection from "../components/sections/search-section/search-section"
+import GoodsSection from "../components/sections/goods-section/goods-section"
+
+
+function GoodsItemPage() {
+  return(
+    <>
+      <HeaderSection/>
+      <FooterSection/>
+    </>
+  )
+}
+
+export default GoodsItemPage
