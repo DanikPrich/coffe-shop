007f3992e545dbe5f729943f03baa98c79065ed1
Daniel <Danil.danek@gmail.com>
2023-03-25 13:47:09 +0100

    search section

--- src/pages/coffe-page/coffe-page.js
+++ src/pages/coffe-page/coffe-page.js
@@ -2,6 +2,7 @@
 import HeaderSection from "../../components/sections/header-section/header-section"
 import FooterSection from "../../components/sections/footer-section/footer-section"
 import AboutSection from "../../components/sections/about-section/about-section"
+import SearchSection from "../../components/sections/search-section/search-section"
 
 
 function CoffePage() {
@@ -9,6 +10,7 @@ function CoffePage() {
     <>
       <HeaderSection/>
       <AboutSection/>
+      <SearchSection/>
       <FooterSection/>
     </>
   )
